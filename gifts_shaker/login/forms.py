f7deab49d31b30d6eaeb049752d9e866d0eac7f8
mkrzeminski <mkrzeminski@imgw.pl>
2022-11-07 23:25:25 +0100

    Registration and Login
    Registration is correct but login works only via username (not email)

---
+++ gifts_shaker/login/forms.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+
+class CreateUserForm(UserCreationForm):
+    email = forms.EmailField(
+        max_length=100,
+        required=True,
+        help_text='Enter Email Address',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+    )
+    first_name = forms.CharField(
+        max_length=100,
+        required=True,
+        help_text='Enter First Name',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
+    )
+    last_name = forms.CharField(
+        max_length=100,
+        required=True,
+        help_text='Enter Last Name',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
+    )
+    container = forms.CharField(
+        max_length=200,
+        required=False,
+        help_text='Enter Group name',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Container Name'}),
+    )
+    username = forms.CharField(
+        max_length=200,
+        required=False,
+        help_text='Enter Username',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+    )
+    password1 = forms.CharField(
+        help_text='Enter Password',
+        required=True,
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+    )
+    password2 = forms.CharField(
+        required=True,
+        help_text='Enter Password Again',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password Again'}),
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
--- gifts_shaker/login/views.py
+++ gifts_shaker/login/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import CreateUserForm
+
+
+def register_page(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+
+                return redirect('login')
+
+        context = {'form': form}
+        return render(request, 'register.html', context)
+
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm(request.POST)
+        if request.method == 'POST':
+            email = request.POST.get('email')
+            password = request.POST.get('password1')
+            print(email, password)
+            user = authenticate(request, username=email, password=password)
+            print(user)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+        context = {'form': form}
+        return render(request, 'login.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
+def home(request):
+    return render(request, 'home.html')
