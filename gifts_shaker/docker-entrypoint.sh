b062b344708d314f350ef5ee9cbfc43aa01ff97d
mkrzeminski <mkrzeminski@imgw.pl>
2024-12-06 16:30:24 +0100

    path

--- gifts_shaker/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-echo "Apply database migrations"
-
-python manage.py makemigrations
-python manage.py migrate
-
-# Start server
-echo "Starting server"
-python manage.py runserver 0.0.0.0:8080
--- gifts_shaker/gifts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from gifts.models import Invitation, Shaker, Gift, Pairs
-
-admin.site.register(Invitation)
-admin.site.register(Shaker)
-admin.site.register(Gift)
-admin.site.register(Pairs)
